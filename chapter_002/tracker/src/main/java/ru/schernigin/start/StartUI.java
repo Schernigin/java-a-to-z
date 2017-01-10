004c28c3ff43da54fa0f68c842cab1b3bd7d6853
Sergey Chernigin <Sergey_Chernigin@inbox.ru>
2017-01-10 11:05:32 +0500

    class StartUI

---
+++ chapter_002/tracker/src/main/java/ru/schernigin/start/StartUI.java
@@ -0,0 +1,14 @@
+package ru.schernigin.start;
+
+import ru.schernigin.models.*;
+
+public class StartUI {
+
+	public static void main(String[] args){
+		Tracker tracker = new Tracker();
+		tracker.add(new Task("first task", "first desc"));
+		for (Item item : tracker.getAll()) {
+			System.out.println(item.getName());
+		}
+	}
+}
