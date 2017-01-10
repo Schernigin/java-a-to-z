fb1dc02d0ce81d2cb4ad076ee237594817e16e9a
Sergey Chernigin <Sergey_Chernigin@inbox.ru>
2017-01-10 11:07:43 +0500

    class Task

---
+++ chapter_002/tracker/src/main/java/ru/schernigin/models/Task.java
@@ -0,0 +1,17 @@
+package ru.schernigin.models;
+
+/**
+ * class Task.
+ * @author schernigin.
+ * @sienc 15.12.2016
+ * @version 1.0
+ */
+
+public class Task extends Item {
+
+	public Task(String name, String desc) {
+		this.name = name;
+		this. description = desc;
+	}
+
+}
