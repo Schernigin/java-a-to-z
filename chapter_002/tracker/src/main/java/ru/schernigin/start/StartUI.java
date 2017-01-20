c26684ea998d734f0ea6d9317cee1eb21b573ed6
Sergey Chernigin <Sergey_Chernigin@inbox.ru>
2017-01-20 14:52:56 +0500

    StrartUI class

--- chapter_002/tracker/src/main/java/ru/schernigin/start/StartUI.java
+++ chapter_002/tracker/src/main/java/ru/schernigin/start/StartUI.java
@@ -5,9 +5,33 @@ import ru.schernigin.models.*;
 public class StartUI {
 	private Input input;
 	
+	private String inMenu;
+	
+	private String[] menu = {"Hello! MENU:", "1 New item", "2 show all items", "3 update item", "4 find the item on behalf of the", "5 find the item on id of the", "6 find the item on the description", "7 delete item", "8 exsit"};
+	
 	public StartUI(Input input) {
 		this.input = input;
+	}	
+			
+	public String showMenu(){
+		for(String i : this.menu) {
+			System.out.println(i);
+		}
+		this.inMenu = input.ask("select the menu item, eg: 3");		
+		return this.inMenu;
 	}
+	
+	public void select() {
+		for(String i : this.menu) {
+			boolean firstValue;
+			if(firstValue) {
+				firstValue = this.menu[i].startsWith("1");
+				new StartUI(input).init();
+			}	
+		}
+	}
+		
+
 	
 	public void init() {
 		String name = input.ask("Please enter the taks's name: ");
@@ -16,11 +40,10 @@ public class StartUI {
 		for (Item item : tracker.getAll()) {
 			System.out.println(item.getName());
 		}	
-	}
+	} 
 
 	public static void main(String[] args)  {
-		//Input input = new StubInput(new String[] {"creat stub task"})
 		Input input = new ConsoleInput();
-		new StartUI(input).init();
+		new StartUI(input).showMenu();
 	}
 }
