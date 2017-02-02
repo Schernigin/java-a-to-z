0bda01142d5211e0e6a19ae5b38a026a523bc5fe
Sergey Chernigin <Sergey_Chernigin@inbox.ru>
2017-02-02 12:08:51 +0500

    Class ConvertIterator

--- chapter_002/tracker/src/main/java/ru/schernigin/services/ConvertIterator.java
+++ chapter_002/tracker/src/main/java/ru/schernigin/services/ConvertIterator.java
@@ -1,6 +1,10 @@
-//package ru.schernigin.services;
+package ru.schernigin.services;//package ru.schernigin.services;
 
 //import java.util.Iterator;
+
+import ru.schernigin.services.UseIterator;
+
+import java.util.Iterator;
 
 /**
  * CLass ConvertIterator.
@@ -8,21 +12,48 @@
  * @since 31.01.2017
  * @version 1.0
  */
-//public class ConvertIterator implements UseIterator, Integer<Integer> {
+public class ConvertIterator implements UseIterator, Iterator<Integer> {
 
     /**
      * external iterator
      */
 
- //   private Iterator<Iterator<Integer>> it;
+    private Iterator<Iterator<Integer>> it;
 
     /**
      * inner iterator
      */
 
-  //  private Iterator<Integer> inner;
+    private Iterator<Integer> inner;
 
-  //  public  Iterator<Integer> convert(Iterator<Iterator<Integer>>it){
-   //     return null;
-   // }
-//}
+    public  Iterator<Integer> convert(Iterator<Iterator<Integer>>it){
+        this.it = it;
+        if (it.hasNext()) {
+            this.inner = it.next();
+        }
+        return this;
+    }
+
+
+    @Override
+    public boolean hasNext() {
+        return it.hasNext() || inner.hasNext();
+    }
+
+    @Override
+    public Integer next() {
+        Integer result;
+            if (inner.hasNext()) {
+                result = inner.next();
+            } else {
+                inner = it.next();
+                result = inner.next();
+            }
+        return result;
+    }
+
+    @Override
+    public void remove() {
+
+    }
+}
